3e6234f917145bced8941d52be35f32feaa8c122
Chang Wang <garnwraly@gmail.com>
2017-06-09 15:26:54 -0400

    Page Object - add selectors for Home > Data Summary and Common > loading indicator

--- src/support/pages/GDC/common.page.js
+++ src/support/pages/GDC/common.page.js
@@ -6,7 +6,8 @@ class CommonPage {
     get selectors () {
       return {
         privacy_warning: 'body > div.ReactModalPortal > div > div',
-        accept_privacy_warning_button: 'body > div.ReactModalPortal > div > div > div > div:nth-child(3) > button'
+        accept_privacy_warning_button: 'body > div.ReactModalPortal > div > div > div > div:nth-child(3) > button',
+        load_indicator: '#root > div > div.css-co82fs > div',
       };
     }
 }
--- src/support/pages/GDC/home.page.js
+++ src/support/pages/GDC/home.page.js
@@ -8,6 +8,12 @@ class HomePage {
     return {
       search_box: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-13azwyo > input',
       search_results: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-13azwyo > ul',
+      summary_project_count: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-12d3q5d > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span > span',
+      summary_primary_sites_count: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-12d3q5d > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > span > span',
+      summary_cases_count: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-12d3q5d > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > span > span',
+      summary_files_count: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-12d3q5d > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > span > span',
+      summary_genes_count: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-12d3q5d > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > span > span',
+      summary_mutations_count: '#skip > div > div.css-1w3mo3h > div:nth-child(1) > div.css-12d3q5d > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > div:nth-child(2) > span > span',
     };
   }
 
