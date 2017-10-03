d0abcf8ebec591b6a013e61425a773d33bf92883
Justin Barnowski <jbarno@uchicago.edu>
2017-10-03 14:28:31 -0500

    Allure works, but treats steps as tests

--- wdio.BUILD.conf.js
+++ wdio.BUILD.conf.js
@@ -1,6 +1,12 @@
 const config = require('./wdio.conf.js').config;
 
-config.maxInstances = 5;
+config.reporters = ['allure', 'dot', 'spec', 'json'];
+config.reporterOptions = {
+    outputDir: './wdio-logs/',
+    allure: {
+        outputDir: './allure-reports/allure/'
+    }
+};
 
 config.services = ['selenium-standalone'];
 
